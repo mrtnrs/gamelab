66aaa2dc9aa81fadd62870e58e78f3788043c9d8
mrtnrs <github@gzt.be>
2025-03-03 17:46:37 +0100

    next 15

---
+++ .open-next/env/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://ifosxcolqtgtyprwnruv.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlmb3N4Y29scXRndHlwcnducnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMDA0MTEsImV4cCI6MjA1NjU3NjQxMX0.zU_yYGrj4X2dtbC2KIwBp8FjbaZV6Ps7Qd6vbmF2JZY"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://ifosxcolqtgtyprwnruv.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlmb3N4Y29scXRndHlwcnducnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMDA0MTEsImV4cCI6MjA1NjU3NjQxMX0.zU_yYGrj4X2dtbC2KIwBp8FjbaZV6Ps7Qd6vbmF2JZY"};
+export const test = {};
